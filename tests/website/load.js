334cda2d3624dcf1100cc894eb8213ca957d457b
Anton Winkler <antonwinkler98@web.de>
2022-12-04 15:43:13 +0100

    update

--- tests/website/load.js
+++ tests/website/load.js
@@ -6,7 +6,8 @@ const BASE_PATH = "https://nibuja05.github.io/AOC_CI_test/";
 async function loadDataForDay(day) {
 	try {
 		const request = await fetch(BASE_PATH + `data/${zeroPad(day, 2)}.json`);
-		return await request.json();
+		const result = await request.json();
+		return result;
 	} catch (error) {
 		console.log(error);
 		console.log(`No existing data for ${day}`);
@@ -15,12 +16,12 @@ async function loadDataForDay(day) {
 }
 
 async function loadDays() {
-	loadDataForDay(2);
-	// for (let i = 1; i <= 24; i++) {
-	// 	dayData = loadDataForDay(i);
+	// loadDataForDay(1);
+	for (let i = 1; i <= 24; i++) {
+		dayData = await loadDataForDay(i);
 
-	// 	createDay(i, dayData);
-	// }
+		createDay(i, dayData);
+	}
 }
 
 function createDay(index, data) {
@@ -29,11 +30,26 @@ function createDay(index, data) {
 	newDay.classList.add("day");
 	nav.appendChild(newDay);
 	const dayLink = document.createElement("a");
-	dayLink.href = `day${index}`;
+	dayLink.href = `#day${index}`;
 	dayLink.innerHTML = index;
 	newDay.appendChild(dayLink);
 
 	if (!data) newDay.classList.add("inactive");
+	console.log(data);
+
+	const resultContainer = document.getElementById("content");
+	const result = document.createElement("div");
+	result.classList.add("result");
+	result.id = `day${index}-data`;
+	resultContainer.appendChild(result);
+	const anchor = document.createElement("div");
+	anchor.classList.add("anchor");
+	anchor.id = `day${index}`;
+	result.appendChild(anchor);
+
+	const header = document.createElement("h2");
+	header.innerHTML = `Day ${index}`;
+	result.appendChild(header);
 }
 
 loadDays();
