be34d1b1fb2f741bbcfb7680967306d5794c88e8
Nibuja <aw93mita@studserv.uni-leipzig.de>
2022-12-04 14:11:21 +0100

    update

--- tests/website/load.js
+++ tests/website/load.js
@@ -1,11 +1,20 @@
 const zeroPad = (num, places) => String(num).padStart(places, "0");
 
-function loadDays() {
+const BASE_PATH =
+	"file:///C:/Users/anton/Programmieren/AOC_CI_test/tests/website/";
+
+async function loadDataForDay(day) {
+	return {};
+	const request = await fetch(BASE_PATH + `../data/${zeroPad(day, 2)}.json`);
+}
+
+async function loadDays() {
 	for (let i = 1; i <= 24; i++) {
 		dayData = {};
-		const t = import("../data/01.json");
+		// const t = import(BASE_PATH + "../data/01.json");
+
 		try {
-			dayData = require(`../data/${zeroPad(i, 2)}.json`);
+			dayData = loadDataForDay(i);
 		} catch (error) {
 			console.log("not existing: ", i);
 		}
