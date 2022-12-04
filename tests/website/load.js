51f876cf0aad10dd3322f76877d0754d2dd1041f
Anton Winkler <antonwinkler98@web.de>
2022-12-04 15:08:44 +0100

    update

--- tests/website/load.js
+++ tests/website/load.js
@@ -1,11 +1,12 @@
 const zeroPad = (num, places) => String(num).padStart(places, "0");
 
+// const BASE_PATH ="file:///C:/Users/anton/Programmieren/AOC_CI_test/tests/website/";
 const BASE_PATH =
 	"file:///C:/Users/anton/Programmieren/AOC_CI_test/tests/website/";
 
 async function loadDataForDay(day) {
-	return {};
-	const request = await fetch(BASE_PATH + `../data/${zeroPad(day, 2)}.json`);
+	// return {};
+	const request = await fetch(BASE_PATH + `data/${zeroPad(day, 2)}.json`);
 }
 
 async function loadDays() {
