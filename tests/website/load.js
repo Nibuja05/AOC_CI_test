e631d339d717a174f00aefd567931d504f60fdaf
Anton Winkler <antonwinkler98@web.de>
2022-12-04 15:49:51 +0100

    update

--- tests/website/load.js
+++ tests/website/load.js
@@ -24,32 +24,43 @@ async function loadDays() {
 	}
 }
 
-function createDay(index, data) {
+function prepareDays() {
 	const nav = document.getElementById("nav");
-	const newDay = document.createElement("div");
-	newDay.classList.add("day");
-	nav.appendChild(newDay);
-	const dayLink = document.createElement("a");
-	dayLink.href = `#day${index}`;
-	dayLink.innerHTML = index;
-	newDay.appendChild(dayLink);
+	for (let i = 1; i <= 24; i++) {
+		const newDay = document.createElement("div");
+		newDay.classList.add("day");
+		newDay.id = `nav-day${i}`;
+		nav.appendChild(newDay);
+		const dayLink = document.createElement("a");
+		dayLink.href = `#day${i}`;
+		dayLink.innerHTML = i;
+		newDay.appendChild(dayLink);
 
-	if (!data) newDay.classList.add("inactive");
-	console.log(data);
-
-	const resultContainer = document.getElementById("content");
-	const result = document.createElement("div");
-	result.classList.add("result");
-	result.id = `day${index}-data`;
-	resultContainer.appendChild(result);
-	const anchor = document.createElement("div");
-	anchor.classList.add("anchor");
-	anchor.id = `day${index}`;
-	result.appendChild(anchor);
-
-	const header = document.createElement("h2");
-	header.innerHTML = `Day ${index}`;
-	result.appendChild(header);
+		newDay.classList.add("inactive");
+	}
 }
 
+function createDay(index, data) {
+	const newDay = document.getElementById(`nav-day${index}`);
+
+	if (data && Object.keys(data).length > 0) {
+		newDay.classList.remove("inactive");
+
+		const resultContainer = document.getElementById("content");
+		const result = document.createElement("div");
+		result.classList.add("result");
+		result.id = `day${index}-data`;
+		resultContainer.appendChild(result);
+		const anchor = document.createElement("div");
+		anchor.classList.add("anchor");
+		anchor.id = `day${index}`;
+		result.appendChild(anchor);
+
+		const header = document.createElement("h2");
+		header.innerHTML = `Day ${index}`;
+		result.appendChild(header);
+	}
+}
+
+prepareDays();
 loadDays();
